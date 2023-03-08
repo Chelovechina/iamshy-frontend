4280436bb002380bdfcaddeb07e08951e6238625
Chelovechina <zkk2004@gmail.com>
2023-03-08 14:55:30 +0600

    feat: implement login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,12 +1,31 @@
-import { FC } from "react"
-import "./login.scss" 
+import { FC } from "react";
+import "./login.scss";
 
 const Login: FC = () => {
   return (
     <div className="login">
-      Login
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
+            alias totam numquam ipsa exercitationem dignissimos, error nam,
+            consequatur.
+          </p>
+          <span>Don't you have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h2>Login</h2>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
