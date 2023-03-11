0f8af213c1c37404e9bc2125d2c3662ded1939a4
Chelovechina <zkk2004@gmail.com>
2023-03-11 12:08:05 +0600

    feat: implement NavBar component

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,8 +1,46 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import { Link } from "react-router-dom";
+
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import GridViewOutlineIcon from "@mui/icons-material/GridViewOutlined";
+import NotificationsOutlineIcon from "@mui/icons-material/NotificationsOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlineIcon from "@mui/icons-material/SearchOutlined";
+
 import "./navBar.scss";
+import userImg from "./../../assets/login.jpeg";
 
 const NavBar: FC = () => {
-  return <div>NavBar</div>;
+  const [isDark, setIsDark] = useState<boolean>(false);
+
+  return (
+    <div className="navBar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span>I'am shy</span>
+        </Link>
+        <HomeOutlinedIcon />
+        {isDark ? <WbSunnyOutlinedIcon /> : <DarkModeOutlinedIcon />}
+        <GridViewOutlineIcon />
+        <div className="search">
+          <SearchOutlineIcon />
+          <input type="text" placeholder="Поиск" />
+        </div>
+      </div>
+      <div className="right">
+        <PersonOutlineIcon />
+        <EmailOutlinedIcon />
+        <NotificationsOutlineIcon />
+        <div className="user">
+          <img src={userImg} alt="Пользователь" />
+          <span>Иван Иванов</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
